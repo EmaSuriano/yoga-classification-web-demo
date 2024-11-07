d8eae44f033678cc9044c7bf605e8861404667be
Emanuel Suriano <emanuel.suriano@revolut.com>
2024-11-07 08:34:54 +0100

    update utils

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import base64
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
+import tensorflow as tf
 
 
 def set_background(image_file):
@@ -28,7 +29,9 @@ def classify(image, model, class_names):
     image_array = np.asarray(image)
 
     # normalize image
-    normalized_image_array = image_array.astype(np.float32) / 255
+    normalized_image_array = tf.keras.applications.efficientnet.preprocess_input(
+        image_array.astype(np.float32)
+    )
 
     # set model input
     data = np.ndarray(shape=(1, 128, 128, 3), dtype=np.float32)
@@ -36,8 +39,8 @@ def classify(image, model, class_names):
 
     # make prediction
     prediction = model.predict(data)
+
     index = np.argmax(prediction)
-    # index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
